86e41d06495df1389c4eb6155e4de7731bcac3fd
Jeromy Fu <fuji246@gmail.com>
2018-11-23 20:47:02 -0800

    switch between wifi ap and client

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
+install -v -m 644 files/interfaces.ap           "${ROOTFS_DIR}/etc/network/interfaces.ap"
+install -v -m 644 files/interfaces.client       "${ROOTFS_DIR}/etc/network/interfaces.client"
+install -v -m 644 files/interfaces.ap           "${ROOTFS_DIR}/etc/network/interfaces"
+
+install -v -m 755 files/wifi_switch.sh          "${ROOTFS_DIR}/sbin/wifi_switch.sh"
+
+install -v -m 600 files/hostapd.conf            "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
+install -v -m 600 files/dnsmasq.conf            "${ROOTFS_DIR}/etc/dnsmasq.conf"
 
 install -v -d					"${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d"
 install -v -m 644 files/wait.conf		"${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/"
