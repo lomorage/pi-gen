64675bd46730dc370641cb6a355de96e9277ceea
Jeromy Fu <fuji246@gmail.com>
2019-01-16 23:43:13 -0800

    fix mount issue

--- stage4/00-install-packages/00-run.sh
+++ stage4/00-install-packages/00-run.sh
@@ -3,13 +3,11 @@
 install -v -m 644 files/lightdm.conf                     "${ROOTFS_DIR}/etc/lightdm/lightdm.conf"
 install -v -m 644 files/autostart                        "${ROOTFS_DIR}/etc/xdg/lxsession/LXDE/autostart"
 
-on_chroot << EOF
-sed -ie 's/mount_on_startup=1/mount_on_startup=0/g' /home/pi/.config/pcmanfm/LXDE/pcmanfm.conf
-sed -ie 's/mount_removable=1/mount_removable=0/g' /home/pi/.config/pcmanfm/LXDE/pcmanfm.conf
-sed -ie 's/autorun=1/autorun=0/g' /home/pi/.config/pcmanfm/LXDE/pcmanfm.conf
-EOF
+install -v -d                                            "${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE/"
+install -v -m 644 files/pcmanfm.conf                     "${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE/pcmanfm.conf"
 
 install -v -m 755 files/install_magic_mirror.sh          "${ROOTFS_DIR}/sbin/install_magic_mirror.sh"
+
 on_chroot << EOF
 /sbin/install_magic_mirror.sh
 EOF
