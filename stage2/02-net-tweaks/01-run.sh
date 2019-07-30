d6a707be8ef1fe2c5bcecc3f6850f47c75ef8c8f
Jeromy Fu <fuji246@gmail.com>
2019-07-29 23:26:36 -0700

    use wired connection by default

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -2,7 +2,7 @@
 install -v -m 644 files/interfaces.ap           "${ROOTFS_DIR}/etc/network/interfaces.ap"
 install -v -m 644 files/interfaces.client       "${ROOTFS_DIR}/etc/network/interfaces.client"
 install -v -m 644 files/interfaces.eth          "${ROOTFS_DIR}/etc/network/interfaces.eth"
-install -v -m 644 files/interfaces.ap           "${ROOTFS_DIR}/etc/network/interfaces"
+install -v -m 644 files/interfaces.eth           "${ROOTFS_DIR}/etc/network/interfaces"
 
 install -v -m 755 files/wifi_switch.sh          "${ROOTFS_DIR}/sbin/wifi_switch.sh"
 
@@ -12,9 +12,13 @@ install -v -m 600 files/dnsmasq.conf            "${ROOTFS_DIR}/etc/dnsmasq.conf"
 install -v -d					"${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d"
 install -v -m 644 files/wait.conf		"${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/"
 
+install -v -d                                   "${ROOTFS_DIR}/etc/systemd/system/networking.service.d"
+install -v -m 644 files/reduce-timeout.conf     "${ROOTFS_DIR}/etc/systemd/system/networking.service.d/"
+
 install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
 
 on_chroot << EOF
-systemctl enable hostapd
+systemctl disable hostapd
+systemctl disable dnsmasq
 EOF
