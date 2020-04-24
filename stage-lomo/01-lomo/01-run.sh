1d48693c8e6f75d67c5778520c21072647ce0cae
Jeromy Fu <fuji246@gmail.com>
2020-04-24 15:13:55 -0700

    fix network configure

--- stage-lomo/01-lomo/01-run.sh
+++ stage-lomo/01-lomo/01-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+install -m 644 "${ROOTFS_DIR}/etc/network/interfaces.eth" "${ROOTFS_DIR}/etc/network/interfaces"
 install -m 644 files/logo_transparent.png "${ROOTFS_DIR}/usr/share/plymouth/debian-logo.png"
 install -m 644 files/spinfinity.script    "${ROOTFS_DIR}/usr/share/plymouth/themes/spinfinity/"
 install -m 644 files/spinfinity.plymouth  "${ROOTFS_DIR}/usr/share/plymouth/themes/spinfinity/"
