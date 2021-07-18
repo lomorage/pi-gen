5aeb3633e1b6f6bb34ea9a0f3776a76bfe89cf1d
Jeromy Fu <fuji246@gmail.com>
2021-07-17 23:28:08 -0700

    use subdomain for lomo repo

--- stage-lomo/01-lomo/01-run.sh
+++ stage-lomo/01-lomo/01-run.sh
@@ -4,7 +4,6 @@ install -m 644 "${ROOTFS_DIR}/etc/network/interfaces.eth" "${ROOTFS_DIR}/etc/net
 install -m 644 files/logo_transparent.png "${ROOTFS_DIR}/usr/share/plymouth/debian-logo.png"
 install -m 644 files/spinfinity.script    "${ROOTFS_DIR}/usr/share/plymouth/themes/spinfinity/"
 install -m 644 files/spinfinity.plymouth  "${ROOTFS_DIR}/usr/share/plymouth/themes/spinfinity/"
-install -m 755 files/update-lomod         "${ROOTFS_DIR}/etc/cron.daily/"
 
 on_chroot << EOF
 plymouth-set-default-theme -R spinfinity
